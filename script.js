4c4c264264fa6685bd8d76543107ad29ca735444
victor-prm <victorpabb@gmail.com>
2025-01-30 13:30:46 +0100

    only missing one task

--- script.js
+++ script.js
@@ -1,2 +1,49 @@
 // Lav dine funktioner her:
 
+function assignmentA(a,b){
+    let testBool = false;
+    console.log("Assignment A says:")
+
+    if(a === b){
+        testBool = true;
+        console.log("These values are the same")
+    }else{
+        console.log("These values are NOT the same")
+    }
+    return testBool
+}
+
+function assignmentB(str){
+    console.log("Assignment B says:")
+    str = str.slice(-3)
+    console.log(str);
+    return str;
+}
+
+function assignmentC(num){
+    console.log("Assignment C says:")
+    let isEven = num % 2 == 0 ? true : false; 
+    console.log(`Is ${num} even? ${isEven}`)
+    return isEven;
+}
+
+function assignmentD(arr){
+    console.log("Assignment D says:")
+    arr = arr.sort(function(a, b) {
+        return a - b;
+    });
+    let firstValueOverZero = arr.find((element) => element > 0);
+    console.log(firstValueOverZero);
+}
+
+function assignmentE(arr){
+    console.log("Assignment E says:")
+    let bArray = arr.filter(name => name.toUpperCase().startsWith("B"));
+
+    if(!bArray.length){
+        bArray = false;
+    }
+    console.log(bArray)
+    return bArray
+   
+}
